fcc6bc912f3358b1de1249fe33eaa1e11ee230f3
Putta <Jithendar.Putta@parexel.com>
2022-03-02 15:29:13 +0530

    intial commit

---
+++ WebApplication1/Models/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Class2
+    {
+      
+
+       
+        
+       
+        public int Id { get; set; }
+        public string FirstName { get; set; }       
+        public string LastName { get; set; }
+
+    }
+}
